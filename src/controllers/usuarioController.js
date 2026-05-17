2d14c4f1dcb21d48140ce9791e1fc8d5b67a7019
gui-perez <guilherme.perez@sptech.school>
2026-05-17 00:46:59 -0300

    adicionando campo cadastro

--- src/controllers/usuarioController.js
+++ src/controllers/usuarioController.js
@@ -1,5 +1,4 @@
 var usuarioModel = require("../models/usuarioModel");
-var aquarioModel = require("../models/aquarioModel");
 
 function autenticar(req, res) {
     var email = req.body.emailServer;
@@ -22,13 +21,14 @@ function autenticar(req, res) {
 
                         aquarioModel.buscarAquariosPorEmpresa(resultadoAutenticar[0].empresaId)
                             .then((resultadoAquarios) => {
+
                                 if (resultadoAquarios.length > 0) {
                                     res.json({
                                         id: resultadoAutenticar[0].id,
                                         email: resultadoAutenticar[0].email,
                                         nome: resultadoAutenticar[0].nome,
                                         senha: resultadoAutenticar[0].senha,
-                                        aquarios: resultadoAquarios
+                    
                                     });
                                 } else {
                                     res.status(204).json({ aquarios: [] });
