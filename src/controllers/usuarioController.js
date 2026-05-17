326e4d48434030310149ac57da9c65c7c0dc341f
gui-perez <guilherme.perez@sptech.school>
2026-05-17 01:02:10 -0300

    corrigindo usuarioController

--- src/controllers/usuarioController.js
+++ src/controllers/usuarioController.js
@@ -16,24 +16,13 @@ function autenticar(req, res) {
                     console.log(`\nResultados encontrados: ${resultadoAutenticar.length}`);
                     console.log(`Resultados: ${JSON.stringify(resultadoAutenticar)}`); // transforma JSON em String
 
-                    if (resultadoAutenticar.length == 1) {
-                        console.log(resultadoAutenticar);
+                    if (resultado.length == 1) {
+                    res.json({
+                        id: resultadoAutenticar[0].id,
+                        nome: resultadoAutenticar[0].nome,
+                        email: resultadoAutenticar[0].email
+                    });
 
-                        aquarioModel.buscarAquariosPorEmpresa(resultadoAutenticar[0].empresaId)
-                            .then((resultadoAquarios) => {
-
-                                if (resultadoAquarios.length > 0) {
-                                    res.json({
-                                        id: resultadoAutenticar[0].id,
-                                        email: resultadoAutenticar[0].email,
-                                        nome: resultadoAutenticar[0].nome,
-                                        senha: resultadoAutenticar[0].senha,
-                    
-                                    });
-                                } else {
-                                    res.status(204).json({ aquarios: [] });
-                                }
-                            })
                     } else if (resultadoAutenticar.length == 0) {
                         res.status(403).send("Email e/ou senha inválido(s)");
                     } else {
@@ -56,7 +45,10 @@ function cadastrar(req, res) {
     var nome = req.body.nomeServer;
     var email = req.body.emailServer;
     var senha = req.body.senhaServer;
-    var fkEmpresa = req.body.idEmpresaVincularServer;
+    var musica = req.body.musicaServer;
+    var fase = req.body.faseServer;
+    var clipe = req.body.clipeServer;
+    var nivel = req.body.nivelServer;
 
     // Faça as validações dos valores
     if (nome == undefined) {
@@ -65,12 +57,18 @@ function cadastrar(req, res) {
         res.status(400).send("Seu email está undefined!");
     } else if (senha == undefined) {
         res.status(400).send("Sua senha está undefined!");
-    } else if (fkEmpresa == undefined) {
-        res.status(400).send("Sua empresa a vincular está undefined!");
-    } else {
+    } else if (musica == undefined) {
+        res.status(400).send("Sua musica está undefined!");
+    } else if (fase == undefined) {
+        res.status(400).send("Sua fase está undefined!");
+    } else if (clipe == undefined) {
+        res.status(400).send("Seu clipe está undefined!");
+    } else if (nivel == undefined) {
+        res.status(400).send("Seu nivel está undefined!");
+    }   else {
 
         // Passe os valores como parâmetro e vá para o arquivo usuarioModel.js
-        usuarioModel.cadastrar(nome, email, senha, fkEmpresa)
+        usuarioModel.cadastrar(nome, email, senha, musica, fase, clipe, nivel)
             .then(
                 function (resultado) {
                     res.json(resultado);
